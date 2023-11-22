1b3e6afde19af8ccbdc4bc713fa6f0fd19cc8b46
KartikCodeVerse <128214174+KartikCodeVerse@users.noreply.github.com>
2023-11-22 17:59:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let bar = document.getElementById("bar");
+let nav = document.getElementById("navbar");
+let close = document.getElementById("close")
+
+// if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+// }
+
+close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
